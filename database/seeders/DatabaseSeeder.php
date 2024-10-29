fc10a64a9683263bd014243d2b7dd2ffe01c39c0
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2024-10-29 22:27:14 +0300

    FEAT: Install and setup Laravel Breeze for authentication

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Shrouk',
+            'email' => 'shrouk@example.com',
+        ]);
+        User::factory()->create([
+            'name' => 'Sara',
+            'email' => 'sara@example.com',
         ]);
     }
 }
