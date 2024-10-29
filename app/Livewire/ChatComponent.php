beacf48edaa646baa503a8c85c59b8ff29e2ecdf
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2024-10-30 00:27:58 +0300

    FEAT: Implement ChatComponent with message loading and display

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -3,13 +3,30 @@
 namespace App\Livewire;
 
 use App\Events\MessageEvent;
+use App\Models\Message;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ChatComponent extends Component
 {
     public $message;
-    public $convo =[];
+    public $convo = [];
+
+    public function mount()
+    {
+        $this->loadMessages();
+    }
+    public function loadMessages()
+    {
+        $messages = Message::with('user')->get();
+        foreach ($messages as $message) {
+            $this->convo[] = [
+                'username' => $message->user->name,
+                'message' => $message->message,
+            ];
+        }
+    }
+
     public function sendMessage() {
         MessageEvent::dispatch(Auth::user()->id, $this->message);
         $this->message='';
--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -1,9 +1,15 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
 
-        <form wire:submit="sendMessage">
-            <x-text-input type="text" wire:model="message"/>
-            <button type="submit">Send</button>
-        </form>
+    <ul>
+        @foreach($convo as $convoItem)
+            <li>{{$convoItem['username']}}:{{$convoItem['message']}}</li>
+        @endforeach
+    </ul>
+
+    <form wire:submit.prevent="sendMessage">
+        <x-text-input type="text" wire:model="message"/>
+        <button type="submit">Send</button>
+    </form>
 
 </div>
